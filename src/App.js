86a0279b1bab7bdd79ce02d5badb40dece517623
Jonathan Rogers <eptgen@gmail.com>
2021-08-02 10:58:25 -0400

    palette search

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import ReactFileReader from 'react-file-reader';
-
+var $ = require("jquery");
 
 function App() {
 	var sixtyFourToString = sixtyfour => {
@@ -17,6 +17,70 @@ function App() {
 		return sixtyfour.slice(sixtyfour.search(",") + 1);
 	}
 	
+	var paletteHeaderMatch = (romString, ind) => {
+		if (romString.charCodeAt(ind) != 0x3F) {
+			return [];
+		}
+		if (romString.length <= ind + 2) {
+			return [];
+		}
+		
+		var begin = romString.charCodeAt(ind + 1);
+		var count = romString.charCodeAt(ind + 2);
+		var result = [0x3F, begin, count];
+		
+		if (romString.length <= ind + count + 2) {
+			return [];
+		}
+		
+		if (begin + count > 0x20) {
+			return [];
+		}
+		
+		if (count < 1) {
+			return [];
+		}
+		
+		for (var i = 0; i < count; i++) {
+			var byt = romString.charCodeAt(ind + i + 3);
+			if (byt > 0x3F) {
+				return [];
+			}
+			result.push(byt);
+		}
+		
+		return result;
+	}
+	
+	var findPalettes = romString => {
+		// return array of `paletteResult` objects, each element in the array corresponds to a different
+		// 3F palette
+		
+		var result = [];
+		
+		for (var i = 0; i < romString.length; i++) {
+			var headerMatch = paletteHeaderMatch(romString, i);
+			if (headerMatch) {
+				result.push({
+					loc: i,
+					data: headerMatch
+				})
+			}
+		}
+		
+		return result;
+	}
+	
+	var getPaletteElement = (palette, prgEnd) => {
+		var result = $("<p>");
+		result.html(palette.data.join(" "));
+		if (palette.loc > prgEnd) {
+			result.css("color", "red");
+		}
+		
+		return result;
+	}
+	
 	var handleFiles = file => {
 		var sixtyfour = trim(file.base64);
 		var filestring = sixtyFourToString(sixtyfour);
@@ -25,9 +89,18 @@ function App() {
 		
 		// console.log(filestring);
 		
-		var printString = "There are " + filestring.charCodeAt(4) + " PRG block(s) and " + filestring.charCodeAt(5) + " CHR block(s) in \"" + filename + "\".";
-		console.log(printString);
-		document.getElementById("desc").innerHTML = printString;
+		var prgOffset = (filestring.charCodeAt(4)) * 16384;
+		var trainerOffset = ((filestring.charCodeAt(6) / 4) % 2) * 512;
+		var header = 16;
+		var prgEnd = prgOffset + trainerOffset + header;
+		
+		var palettes = findPalettes(filestring);
+		
+		for (var i = 0; i < palettes.length; i++) {
+			var paletteDiv = $("#palettes");
+			var paletteElement = getPaletteElement(palettes[i], prgEnd);
+			paletteDiv.append(paletteElement);
+		}
 	}
 	
 	return (
@@ -36,6 +109,8 @@ function App() {
 				<button className='btn'>Upload</button><br/>
 			</ReactFileReader>
 			<p id="desc"></p>
+			<div id="palettes">
+			</div>
 		</div>
 	);
 }
