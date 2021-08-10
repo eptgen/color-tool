3929cb9f65050eea89047a6696332b3af9b66647
Elijah Washington <egw3@williams.edu>
2021-08-10 15:25:22 -0400

    file update

--- src/paletteSearch.js
+++ src/paletteSearch.js
@@ -1,16 +1,16 @@
 
-import reverseVals from './testFunction';
+import stringSearch from './byteSearch';
 
-export default function renderSearch(searchTermsRef, setCurrentPalettes, getNesColor, getTextColor, searchResults, setSearchResults) {
-	
+export default function renderSearch(searchTermsRef, setCurrentPalettes, getNesColor, getTextColor, searchResults, setSearchResults, filebytes) {
+
 	var search = () => {
-		const vals = searchTermsRef.current.value;
-		if (vals === '') return;
-		reverseVals(vals);
-		console.log(reverseVals(vals));
+		const terms = searchTermsRef.current.value;
+		if (terms === '') return;
+		stringSearch(terms, filebytes);
+		console.log(stringSearch(terms, filebytes));
 		searchTermsRef.current.value = null;
 	}
-	
+
 	return (<>
 		<input id="search" ref={searchTermsRef} type="text" placeholder="Search Palettes..." name="search" />
 		<button onClick={search}><i class="fa fa-search"></i>Go</button>
